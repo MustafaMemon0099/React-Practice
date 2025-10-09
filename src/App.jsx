c598501f9fe373a1ed454fdd08542bb10edfcdeb
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-10-09 22:31:33 +0500

    remove change fruit name and added a radio btn and dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ import Skills from './Skills';
 
 
 function App() {
-  const [Fruit, abc] = useState("Apple");
-
-  const changefruit = () => {
-    abc(Fruit === "Apple" ? "Banana" : "Apple" );
-  }
-
+ const [gender , setgender] = useState("Male")
+ const [city , setcity] = useState("")
   return (
     <div>
-      <h1>Change Fruit Name</h1>
-      <h1>{Fruit}</h1>
-      <button className='btn' onClick={changefruit}>Click me!!</button>
+      <input type='radio' name='gender' value = "Male" id='Male' onChange={(e)=>setgender(e.target.value)} chacked = {gender === "Male"}/>
+      <label htmlFor='Male'>Male</label>
+
+      <input type='radio' name='gender' value = "Female" id='Female' onChange={(e)=>setgender(e.target.value)} chacked = {gender === "Female"}/>
+       <label htmlFor='Female'>Female</label>
+
+       <input type='radio' name='gender' value = "prefer not to say" id='prefer not to say' onChange={(e)=>setgender(e.target.value)} chacked = {gender === "prefer not to say"}/>
+      <label htmlFor='prefer not to say'>prefer not to say</label>
+
+       <h2>{gender}</h2>
+       <hr></hr>
+       <select onChange={function(e){setcity (e.target.value)}} defaultValue={"Quetta"}>
+        <option value="Karachi">Karachi</option>
+        <option value="Lahore">Lahore</option>
+        <option value="Islamabad">Islamabad</option>
+        <option value="Quetta">Quetta</option>
+        <option value="Rawalpindi">Rawalpindi</option>
+       </select>
+       <h2>Selected city : {city}</h2>
       <hr></hr>
       <Products/>
       <hr></hr>
