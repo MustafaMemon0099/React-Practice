bfec40fa2c387bf74ec35b1974ec855b12f149bd
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-11-09 22:37:45 +0500

    prac of api calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import FormHandel from './FormHandel'
+import React, { useState } from 'react'
+
 
 function App() {
+  const [finaldata, setFinaldata] = useState('');
+const dog = async function() {
+  const response = await fetch('https://dogapi.dog/api/v2/facts')
+  const data = await response.json();
+  setFinaldata (data.data[0].attributes.body);
+}
+ 
+
   return (
     <div>
-      <FormHandel />
+      <button onClick={dog}>Click me!!</button>
+      <h1>{finaldata}</h1>
+      
     </div>
   )
 }
