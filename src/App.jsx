6828a7a35595925ce6ad7ab3e01bd93b4e9ae398
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-11-11 21:47:34 +0500

    making single page app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Login from './Login.jsx'
+import About from './About.jsx';
+import Product from './Product.jsx';
+import Home from './Home.jsx';
 
 function App() {
-  const [finaldata, setFinaldata] = useState('');
-const dog = async function() {
-  const response = await fetch('https://dogapi.dog/api/v2/facts')
-  const data = await response.json();
-  setFinaldata (data.data[0].attributes.body);
-}
- 
-
   return (
     <div>
-      <button onClick={dog}>Click me!!</button>
-      <h1>{finaldata}</h1>
-      
+      {/* 🧭 Navigation Bar */}
+      <nav>
+        <ul>
+          {/* "Home" button ka route galat tha — usay ab theek kar diya */}
+          <li><Link to="/Home">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>;
+           <li><Link to="/Product">Product</Link></li>;
+            <li><Link to="/About">About</Link></li>
+        </ul>
+      </nav>
+
+      {/* 📍 Routes */}
+      <Routes>
+        {/* Route ka path lowercase me likhna best practice hai */}
+        <Route path="/login" element={<Login />} />;
+        <Route path="/About" element={<About />} />;
+        <Route path="/Product" element={<Product />} />;
+        <Route path="/Home" element={<Home />} />;
+      </Routes>
     </div>
   )
 }
---
+++ src/Product.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Product() {
+  return (
+ <div><h1>Product-Page</h1></div>  )
+}
+
+export default Product
