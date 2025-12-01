589a4af53a2675f006b71068f1bf654bbaece37b
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-12-02 00:50:08 +0500

    make the color maker project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Cart from './Cart'
 import Product from './Product'
 
 function App() {
+  const [r , setR] = useState(255)
+  const [g , setG] = useState(255)
+  const [b , setB] = useState(255)
   return (
     <div>
-        <Navbar/>
-        <Product/>
+       <h1>Color Mixer</h1>
+       <div style={{backgroundColor : 'rgb('+r+' ,'+g+' , '+b+' ) ', height : 300 , width : 300 , marginLeft: 620}}></div>
+       <h3>Red</h3>
+       <input type="range" min ="0" max="255" onChange={ (e) =>setR(e.target.value)} value={r}/>
+       <h3>Green</h3> 
+       <input type="range" min ="0" max="255"  onChange={ (e) =>setG(e.target.value)}  value={g}/> 
+       <h3>Blue</h3>
+       <input type="range" min ="0" max="255" onChange={ (e) =>setB(e.target.value)}  value={b}/> 
     </div>
   )
 }
