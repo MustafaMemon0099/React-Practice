52977b29d9ac2f5baa6c3fffb1bde58548f751c3
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-12-01 23:19:08 +0500

    make more cards with the help of api

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,39 +1,62 @@
-import React from 'react'
-import Cart from './Cart'
-import { useDispatch } from 'react-redux';
-import { increment , decrement ,clear} from './Redux/slice';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, clear } from "./Redux/slice";
 
 function Product() {
-    const Cart = useSelector((state) => state.Cart.count)
-    const dispatch = useDispatch();
+  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const cartCount = useSelector((state) => state.Cart.count);
+
+  // Fetch API data
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      setProducts(data.products); // store array in state
+    }
+    fetchData();
+  }, []);
+
   return (
-
-  <div className="product-card">
-  <img src="https://dlcdnwebimgs.asus.com/gain/FBAAC5C2-2ED4-406C-AA47-5A105797004A/w750/h470/fwebp" className="product-img" />
-
-
-  <div className="product-info">
-    <h2>Wireless Headphones</h2>
-    <h3 className="price">$129.99</h3>
-    <p>
-      Experience high-quality sound with these wireless headphones. Featuring
-      noise cancellation, long-lasting battery life, and a sleek modern design.
-    </p>
-
-    <div className="btn-box">
-      <button className="add-btn" onClick={() => dispatch (increment())}>Add to Cart</button>
-      <button className="remove-btn"  onClick={() => dispatch (decrement())}>Remove From Cart</button>
-       <button className="remove-btn"  onClick={() => dispatch (clear())}>clear From Cart</button>
+    <div>
+      <h1>Products</h1>
+     
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="product-img"
+          />
+          <div className="product-info">
+            <h2>{product.title}</h2>
+            <h3 className="price">${product.price}</h3>
+            <p>{product.description}</p>
+            <div className="btn-box">
+              <button
+                className="add-btn"
+                onClick={() => dispatch(increment())}
+              >
+                Add to Cart
+              </button>
+              <button
+                className="remove-btn"
+                onClick={() => dispatch(decrement())}
+              >
+                Remove From Cart
+              </button>
+              <button
+                className="remove-btn"
+                onClick={() => dispatch(clear())}
+              >
+                Clear Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  </div>
-
-</div>
-
-
-      
-    
-  )
+  );
 }
 
-export default Product
+export default Product;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import { StrictMode } from 'react'
+
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -14,3 +16,4 @@ createRoot(document.getElementById('root')).render(
   </Provider>
   ,
 )
+
