2b61f5550a12f1857834826144c974ff5923e30e
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-12-12 23:10:11 +0500

    prac of api fatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
-import Navbar from './Navbar'
-import Cart from './Cart'
-import Product from './Product'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './index.css'
+
 
 function App() {
-  const [r , setR] = useState(255)
-  const [g , setG] = useState(255)
-  const [b , setB] = useState(255)
+  const [products, setProducts] = useState([]);
+  const fetchdata = async () =>{
+    const response = await axios.get('https://dragonball-api.com/api/characters');
+    setProducts(response.data.items);
+  }
+ 
+
+
   return (
     <div>
-       <h1>Color Mixer</h1>
-       <div style={{backgroundColor : 'rgb('+r+' ,'+g+' , '+b+' ) ', height : 400 , width : 400 , marginLeft: 620 }}></div>
-       <h3>Red</h3>
-       <input type="range" min ="0" max="255" onChange={ (e) =>setR(e.target.value)} value={r}/>
-       <h3>Green</h3> 
-       <input type="range" min ="0" max="255"  onChange={ (e) =>setG(e.target.value)}  value={g}/> 
-       <h3>Blue</h3>
-       <input type="range" min ="0" max="255" onChange={ (e) =>setB(e.target.value)}  value={b}/> 
+      <button onClick={fetchdata}>Fetch Products</button>
+     
+       {products.map(Data => (
+  <div key={Data.id}>
+    <h2>{Data.name}</h2>
+    <p>{Data.gender}</p>
+
+    <div>
+       <img className="character-img" src={Data.image} alt={Data.name} />
+    </div>
+  </div>
+))}
+        
+    
+        
     </div>
   )
 }
---
+++ src/Tanstack Querry/ReactQuery.js
@@ -0,0 +1,12 @@
+// ReactQuery.js
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
